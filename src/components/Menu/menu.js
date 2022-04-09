dffbcc67e8cc3226855c5e3bd3e4b1aae2fe3777
Sanjeev_works <sanjeevmanagutti@gmail.com>
2022-04-09 08:32:08 +0530

    modified media query

--- src/components/Menu/menu.js
+++ src/components/Menu/menu.js
@@ -16,7 +16,7 @@ export default function Menu() {
             <li><a href="#skills">Skills</a></li>
             <li><a href="#work">Work</a></li>
             <li><a href="#experience">Experience</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="#footer">Contact</a></li>
             </ul>
         </div>
 </div>
