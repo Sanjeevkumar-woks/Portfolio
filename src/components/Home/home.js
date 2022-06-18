b41bd5c400cdd2d6ec3ad0b683884d3ff8cb5d28
Sanjeev_works <sanjeevmanagutti@gmail.com>
2022-06-18 16:22:34 +0530

    small changes

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -2,10 +2,6 @@ import React from 'react'
 import './home.css'
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import LogoDevIcon from '@mui/icons-material/LogoDev';
-import TelegramIcon from '@mui/icons-material/Telegram';
 import { init } from "ityped";
 import { useEffect, useRef } from "react";
 import { Paper } from '@mui/material';
@@ -31,10 +27,6 @@ export  function Home() {
         <ul className="social-icons">
           <li><a className="linkedin" aria-label="LinkedIn" href="https://www.linkedin.com/in/sanjeevkumar-managutti-34187a207/"rel='noreferrer' target="_blank"><LinkedInIcon/></a></li> 
           <li><a className="github" aria-label="GitHub" href="https://github.com/Sanjeevkumar-woks" rel='noreferrer'target="_blank"><GitHubIcon/></a></li>
-          <li><a className="twitter" aria-label="Twitter" href="#home" rel='noreferrer' target="_blank"><TwitterIcon/></a></li>
-          <li><a className="telegram" aria-label="Telegram" href="#home" rel='noreferrer' target="_blank"><TelegramIcon/></a></li>
-          <li><a className="instagram" aria-label="Instagram" href="#home"><InstagramIcon/></a></li>
-          <li><a className="dev" aria-label="Dev" href="#home" rel='noreferrer' target="_blank"><LogoDevIcon/></a></li>
         </ul>
       </div>
     </div>
